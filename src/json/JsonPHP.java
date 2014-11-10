8a6d5e107918a18124a701b3586040b1e2dc30dd
Arquimedes <arqui78@gmail.com>
2014-11-10 12:28:16 -0430

    Update JsonPHP.java

--- src/json/JsonPHP.java
+++ src/json/JsonPHP.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import oracle.jrockit.jfr.settings.JSONElement;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONValue;
 import sun.net.www.protocol.http.HttpURLConnection;
